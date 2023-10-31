c44d37343ace15165a83f9ceb0727f53b551531c
kareem abdeen <kareem.elshreef18@gmail.com>
2023-10-31 16:24:30 +0200

    handle shared preference to know user first time in app or not (task not done yet )

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/features/Login/Presentaion/Views/login_view.dart';
-import 'package:food_app/features/onboarding/views/onboarding_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Features/Home/presentation/views/home_view.dart';
+import '../../features/onboarding/views/onboarding_view.dart';
 
 abstract class AppRouter {
   static const konBoardingView = '/';
   static const kHomeView = '/HomeView';
   static const kLoginPage = '/LoginPage';
-
+  static final Future<SharedPreferences> _prefs =
+      SharedPreferences.getInstance();
   static Route<dynamic>? onGenerateRoutes(RouteSettings route) {
     String routeName = route.name!;
+    //Todo: handling shared prefrence=> Dn
 
-    //FIXME: provide the nedded bloc providers here
+    //Todo: provide the nedded bloc providers here
     switch (routeName) {
-      case konBoardingView:
-        return MaterialPageRoute(builder: (context) => const OnBoardingView());
-      case kHomeView:
-        return MaterialPageRoute(builder: (context) => const HomeView());
+      case '/':
+        return MaterialPageRoute(
+          builder: (context) {
+            return FutureBuilder<SharedPreferences>(
+              future: _prefs,
+              builder: (BuildContext context,
+                  AsyncSnapshot<SharedPreferences> snapshot) {
+                if (snapshot.hasData) {
+                  bool firstTimeForUser =
+                      snapshot.data!.getBool('isItFirstTimeForUser') ?? true;
+
+                  if (firstTimeForUser) {
+                    return const OnBoardingView();
+                  } else {
+                    const HomeView();
+                  }
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            );
+          },
+        );
+
       case kLoginPage:
         return MaterialPageRoute(builder: (context) => const LoginView());
+      case kHomeView:
+        return MaterialPageRoute(builder: (context) => const HomeView());
     }
     return null;
   }
--- lib/features/Login/Presentaion/Views/Widgets/login_view_body.dart
+++ lib/features/Login/Presentaion/Views/Widgets/login_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:food_app/core/utils/app_router.dart';
 
+import '../../../../../core/utils/app_router.dart';
 import '../../../../../core/utils/helper.dart';
 import 'custom_button_widget.dart';
 import 'login_description_widget.dart';
