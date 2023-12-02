80ce8b8fd2840dd91ae6ed26833a264305295b34
kareem abdeen <kareem.elshreef18@gmail.com>
2023-12-02 17:44:46 +0200

    add restaurant page file

---
+++ lib/features/Restaurant_page/Presentation/views/restaurant_page_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:food_app/features/Restaurant_page/Presentation/views/widgets/restaurant_view_body.dart';
+
+
+class RestaurantView extends StatelessWidget {
+
+  const RestaurantView({super.key});
+
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return const Scaffold(
+
+      body: RestaurantViewBody(),
+
+    );
+
+  }
+
+}
+
